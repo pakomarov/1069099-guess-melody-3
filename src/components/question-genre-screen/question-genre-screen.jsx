01bc310992d382d9385a0c5df15ca7ece35a44f9
Pavel Komarov <komarov.pa.it@gmail.com>
2020-02-27 15:45:03 +0300

    Добавляет состояние с ответами пользователя в QuestionScreenGenre

--- src/components/question-genre-screen/question-genre-screen.jsx
+++ src/components/question-genre-screen/question-genre-screen.jsx
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types';
 class QuestionGenreScreen extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      userAnswers: this.props.question.answers.fill(false)
+    };
   }
 
   render() {
@@ -47,6 +51,14 @@ class QuestionGenreScreen extends PureComponent {
                   <input className="game__input visually-hidden" type="checkbox" name="answer"
                     value={`answer-${i}`}
                     id={`answer-${i}`}
+                    checked={this.state.userAnswers[i]}
+                    onChange={(evt) => {
+                      const value = evt.target.checked;
+
+                      this.setState((prevState) => {
+                        return {userAnswers: [...prevState.userAnswers.slice(0, i), value, ...prevState.userAnswers.slice(i + 1)]};
+                      });
+                    }}
                   />
                   <label className="game__check" htmlFor={`answer-${i}`}>Отметить</label>
                 </div>
