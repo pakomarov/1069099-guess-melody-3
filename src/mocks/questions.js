f81d055fff3403f9f08bac9051470f310bf69f9e
Pavel Komarov <komarov.pa.it@gmail.com>
2020-03-12 12:05:58 +0300

    Добавляет ссылки на реальные аудио-файлы в моки

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -3,35 +3,35 @@ const AVATAR_URL = `https://api.adorable.io/avatars/128`;
 export default [
   {
     type: `genre`,
-    genre: `rock`,
+    genre: `aids-rock`,
     answers: [{
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `rock`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/9/92/En-AIDS.ogg/En-AIDS.ogg.mp3`,
+      genre: `aids-rock`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `blues`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/9/92/En-AIDS.ogg/En-AIDS.ogg.mp3`,
+      genre: `aids-blues`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `jazz`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/9/92/En-AIDS.ogg/En-AIDS.ogg.mp3`,
+      genre: `aids-jazz`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `rock`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/9/92/En-AIDS.ogg/En-AIDS.ogg.mp3`,
+      genre: `aids-punk`,
     }],
   }, {
     type: `artist`,
     song: {
-      artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      artist: `Jim Aids`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/9/92/En-AIDS.ogg/En-AIDS.ogg.mp3`,
     },
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `John Snow`,
+      artist: `John Aids`,
     }, {
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jack Daniels`,
+      artist: `Jack Aids`,
     }, {
       picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jim Beam`,
+      artist: `Jim Aids`,
     }],
   }
 ];
